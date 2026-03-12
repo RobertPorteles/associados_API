e801b90101bb5a9d692bca96cff32eb7874396d1
RobertPorteles <robertporteless@gmail.com>
2026-03-12 16:14:58 -0300

    estrutura

---
+++ src/main/java/com/br/robertmiler/gerenciamento/domain/entities/Associados.java
@@ -0,0 +1,10 @@
+package com.br.robertmiler.gerenciamento.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Associados")
+public class Associados {
+
+}
