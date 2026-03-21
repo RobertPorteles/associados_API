5fa46decdd5c7c2fd9a1c66a8f52a8bd7f12b3d5
millermvs <millermvs@gmail.com>
2026-03-21 10:35:19 -0300

    feat: refatorar classificacaoFinanceira para usar enum e atualizar DTOs correspondentes

--- src/main/java/com/br/robertmiler/gerenciamento/domain/dtos/response/CargoLiderancaResponseDto.java
+++ src/main/java/com/br/robertmiler/gerenciamento/domain/dtos/response/CargoLiderancaResponseDto.java
@@ -3,13 +3,15 @@ package com.br.robertmiler.gerenciamento.domain.dtos.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.br.robertmiler.gerenciamento.domain.enums.ClassificacaoFinanceira;
+
 @Getter
 @Setter
 public class CargoLiderancaResponseDto {
 
     private Long idCargoLideranca;
     private String nomeCargo;
-    private String classificacaoFinanceira;
+    private ClassificacaoFinanceira classificacaoFinanceira;
     private Boolean ativo;
 
 }
